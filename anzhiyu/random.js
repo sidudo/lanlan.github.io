4dbaaf5273dbd6b10b4065278c8074c7f2f1d49c
zimelan <lan20001222@gmail.com>
2023-04-28 15:24:26 +0800

    Site updated: 2023-04-28 15:24:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/19/UseHexo/","2023/04/19/LOL/LOL BP/","2023/04/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/04/19/hello-world/","2023/04/19/UseHexo/","2023/04/28/Dont Strave/开局流程/","2023/04/19/LOL/LOL BP/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
